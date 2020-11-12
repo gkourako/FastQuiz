66bc4ec1fc23c59b3bfb7d79b3940fc1055fe447
gkourako <georgekourakos4@gmail.com>
2020-11-11 22:22:35 -0500

    Cleaned up code

--- quiz/src/Components/Quiz/script.js
+++ quiz/src/Components/Quiz/script.js
@@ -18,10 +18,8 @@ var questionArr = [
       {answerText: "Yellow Supra", isCorrect: false}, 
       {answerText: "Black Durango", isCorrect: false},
       {answerText: "Silver R34", isCorrect: false}
-    ],
-    
+    ]
   },
-
   {
     questionText: "What is Dom and Letty's famous saying ",
     answerOptions: [
@@ -29,7 +27,7 @@ var questionArr = [
       {answerText: "Ride or Die", isCorrect: true},
       {answerText: "It doesn't matter whether you win by an inch or a mile, winning is winning", isCorrect: false},
       {answerText: "You know this ain't no 10-second race.” “I've got nothing but time.", isCorrect: false}
-    ],
+    ]
   },
   {
     questionText: "In Toyko Drift who says, Life's simple: You make choices and you don't look back.",
@@ -38,7 +36,7 @@ var questionArr = [
       {answerText: "Sean", isCorrect: false},
       {answerText: "D.K.", isCorrect: false},
       {answerText:"Han", isCorrect: true}
-    ],
+    ]
   },
   {
     questionText: "Which movie does Dom look at Letty and say, Ride or die, Remember?",
@@ -47,7 +45,7 @@ var questionArr = [
       {answerText: "Fast 6", isCorrect: true},
       {answerText: "Fate of the Furious", isCorrect: false},
       {answerText: "The Fast and Furious", isCorrect: false}
-    ], 
+    ]
   },
   {
     questionText: "What is Paul Walkers most famous quote?",
@@ -56,7 +54,7 @@ var questionArr = [
       {answerText:"I don’t care what it is in life: listen to your heart. If you do, no matter what, you win.", isCorrect: false},
       {answerText:"I’m not the kind of guy who’s taking advantage of my position.", isCorrect: false},
       {answerText:"You know, all that really matters is that the people you love are happy and healthy. Everything else is just sprinkles on the sundae.", isCorrect: false},
-    ],
+    ]
   }
 ]
 
